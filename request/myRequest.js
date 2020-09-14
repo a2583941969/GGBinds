60d7138a5c2c00660b5c73cb49b70309890a3346
yanglin90356 <903562459@qq.com>
2020-09-14 09:23:41 +0800

    yang

--- request/myRequest.js
+++ request/myRequest.js
@@ -1,7 +1,7 @@
 //设置默认端口号
 const baseUrl ='http://localhost:3000/';
 //封装一个请求的函数
-export const myRequest=(options)={
+export const myRequest=(options)=>{
 	return new Promise((resolve,reject)=>{
 		uni.request({
 			//访问路径
